30863b33e6f05b571ba4df2464ff744577a702e4
Jason Anderson <jasonanderson@JA-MacBook-Air.local>
2024-01-31 22:46:40 -0500

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -11,15 +11,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
+        <meta name="description" content={meta.jasonanderson} />
+        <meta property="og:site_name" content={meta.jasonanderson} />
+        <meta property="og:description" content={meta.jasonanderson} />
+        <meta property="og:title" content={meta.jasonanderson} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:site" content="@j23a" />
+        <meta name="twitter:title" content={meta.j23a} />
+        <meta name="twitter:description" content={meta.j23a} />
         <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
